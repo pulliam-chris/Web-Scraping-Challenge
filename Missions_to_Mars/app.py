8188cc71a2b467dc4a6d1c227372faee712e7c18
Chris Pulliam <chrispulliam.jobs@gmail.com>
2021-03-21 14:49:56 -0700

    staging files for scrape function and flask app

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars"
+mongo = PyMongo(app)
+
+# Or set inline
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/mars")
+
+
+@app.route("/")
+def index():
+    marsinfo = mongo.db.marsinfo.find_one()
+    return render_template("index.html", marsinfo=marsinfo)
+
+
+@app.route("/scrape")
+def scraper():
+    marsinfo = mongo.db.marsinfo
+    mars_data = scrape_mars.scrape()
+    marsinfo.update({}, mars_data, upsert=True)
+    return redirect(url_for('index'), code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
