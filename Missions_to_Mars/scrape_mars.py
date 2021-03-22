a999a288d531a56b8ce515b01429c78f0d8565a0
Chris Pulliam <chrispulliam.jobs@gmail.com>
2021-03-21 19:19:28 -0700

    can't get splinter working with flask. focussing on soup dictionary creation

---
+++ Missions_to_Mars/scrape_mars.py
@@ -0,0 +1,88 @@
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+from webdriver_manager.chrome import ChromeDriverManager
+import requests
+import time
+import os
+#import pymongo
+
+# The default port used by MongoDB is 27017
+# https://docs.mongodb.com/manual/reference/default-mongodb-port/
+#conn = 'mongodb://localhost:27017'
+#client = pymongo.MongoClient(conn)
+
+# Define the 'classDB' database in Mongo
+#db = client.marsinfo
+
+#def init_browser():
+    # Replace the path with your actual path to the chromedriver
+    #executable_path = {"executable_path": "C:/Users/mrpar/.wdm/drivers/chromedriver/win32/89.0.4389.23"}
+    #ChromeDriverManager().install()
+    # "C:/Users/mrpar/.wdm/drivers/chromedriver/win32/89.0.4389.23"
+    # "C:/Users/mrpar/Documents/bootcamp/chromedriver/"
+    #return Browser("chrome", **executable_path, headless=True)
+
+def scrape():
+
+    marsinfo = {}
+    #executable_path = {"executable_path": "C:/Users/mrpar/Documents/bootcamp/chromedriver"}
+    #browser = Browser("chrome", **executable_path, headless=True)
+
+    #base_url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    #browser.visit(base_url)
+    #time.sleep(1)
+
+    #html = browser.html
+    #soup = bs(html, 'html.parser')
+    #image_location = ""
+
+    #for i in soup.find_all('img', class_='headerimage fade-in'):
+        #image_location = (i.get('src'))
+
+    #featured_image_url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/" + image_location
+
+    #db.classroom.insert_one({'featured_image_url' : featured_image_url})
+
+    #marsinfo = {'featured_image_url' : featured_image_url}
+
+    url = "https://mars.nasa.gov/news"
+    html = requests.get(url).text
+
+    # Create a Beautiful Soup object
+    soup = bs(html, 'html.parser')
+
+    allTitleElements = soup.find_all('div', class_='content_title')
+
+    # A blank list to hold the headlines
+    newsTitles = []
+    # Loop over div elementsz
+    for element in allTitleElements:
+        # If element has an anchor...
+        if (element.a):
+            # And the anchor has non-blank text...
+            if (element.a.text):
+                # Append the td to the list
+                newsTitles.append(element)
+
+    allParElements = soup.find_all('div', class_='rollover_description_inner')
+
+    # A blank list to hold the descriptions
+    newsParagraphs = []
+    # Loop over div elements
+    for element in allParElements:
+        # And the element has non-blank text...
+        if (element.text):
+            # Append the text to the list
+            newsParagraphs.append(element)
+
+    #Construct Dictionary of Title w/ Paragraph
+    #headlines = []
+
+    for i in range(len(newsTitles)):
+        package = {}
+        package['news_title'] = newsTitles[i].text.strip()
+        package['news_p'] = newsParagraphs[i].text.strip()
+        #headlines.append(package)
+        marsinfo.update(dict(package))
+
+    return marsinfo
